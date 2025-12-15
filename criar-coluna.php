c42b57f1ad03b47874542455fa666514a540aace
Liliane Paixão <lilianedevpaixao@gmail.com>
2025-12-14 23:18:00 -0300

    Cria coluna para imagem no banco de dados.

---
+++ criar-coluna.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+$dbPath = __DIR__ . '/banco.sqlite';
+$pdo = new PDO("sqlite:$dbPath");
+
+$pdo->exec("ALTER TABLE videos ADD COLUMN image_path TEXT;");
+
+echo "Coluna adicionada com sucesso";
