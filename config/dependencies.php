2960e262b6310b67b82cba110775f5db9a15870f
Liliane Paixão <lilianedevpaixao@gmail.com>
2025-12-23 12:47:54 -0300

    Dependencies Injections with php-pdi

--- config/dependencies.php
+++ config/dependencies.php
@@ -3,14 +3,14 @@
 $builder = new \DI\ContainerBuilder();
 $builder->addDefinitions([
     //Como factory
-    // PDO::class => function () {
-    //     $dbPath = __DIR__ . '/../banco.sqlite';
-    //     return new PDO("sqlite:$dbPath");
-    // },
+    PDO::class => function () {
+        $dbPath = __DIR__ . '/../banco.sqlite';
+        return new PDO("sqlite:$dbPath");
+    },
 
     // como função
-    PDO::class => \DI\create(PDO::class)
-        ->constructor('sqlite:' . __DIR__ . '/../banco.sqlite'),    
+    // PDO::class => \DI\create(PDO::class)
+    //     ->constructor('sqlite:' . __DIR__ . '/../banco.sqlite'),    
 ]);
 
 
